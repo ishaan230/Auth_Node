24cfd58303493d9b892ac86b798d276c0b21ef45
IshaanS <ishaansharma1975@gmail.com>
2024-07-07 15:51:01 +0530

    added cookies, session

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const mongoose = require("mongoose")
 const { title } = require('process')
 const { error } = require('console')
 
+const session = require('express-session')
+const passport = require('passport')
+const passportLocalMongoose = require('passport-local-mongoose')
+
+
 // const encrypt = require('mongoose-encryption')
 const bcrypt = require('bcrypt')
 const saltRounds = 12
@@ -14,18 +19,36 @@ mongoose.connect("mongodb://localhost:27017/userDB")
 
 const userSchema = new mongoose.Schema({
     username:String,
-    password:String
+    password:String,
+    secret:String
 })
+
+userSchema.plugin(passportLocalMongoose)
 
 // userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:['password']})
 
 const User = new mongoose.model("User",userSchema)
+
+passport.use(User.createStrategy())
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 app = express()
 
 app.set('view engine','ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"))
+
+app.use(session({
+    secret: "SecretHere",
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 
 
 app.get("/login",function(req,res){
@@ -35,35 +58,92 @@ app.get("/register",function(req,res){
     res.render("register")
 })
 app.get("/",function(req,res){
-    res.render("home")
+    if(req.isAuthenticated()){
+        res.redirect("secret")
+    }else{
+        res.render("home")
+    }
+
 })
+
+app.post("/submit",function(req,res){
+    var secretText = req.body.secret
+    console.log(secretText)
+    console.log(req.user)
+    User.findById(req.user._id).then((foundUser)=>{
+        foundUser.secret = secretText
+        foundUser.save()
+        res.redirect('/secret')
+        // console.log(foundUser)
+    })
+})
+
+app.get("/secret",function(req,res){
+    User.find({secret: {$ne:null}}).then((foundUser) => {
+        res.render("secrets",{userswithSecret:foundUser})
+        console.log("done")
+})
+})
+
+
+app.get("/submit",function(req,res){
+    if(req.isAuthenticated()){
+        res.render("submit")
+    }else{
+        res.render("home")
+    }
+})
+
 
 app.post("/register",function(req,res){
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const userDetails = new User({
-            username:req.body.username,
-            password:hash
+    User.register({username: req.body.username},req.body.password,function(err,user){
+      if(err){
+        console.log(err)
+        res.redirect("/register")
+      }else{
+        passport.authenticate("local")(req,res,function(){
+            res.redirect("/secret")
         })
-        userDetails.save()
-        console.log('Done!')
-        res.render("login")
-    });
+      }  
+    })
+})
+
+app.post
+
+app.get("/secret",function(req,res){
+    if(req.isAuthenticated()){
+        res.render("secrets")
+    }else{
+        res.redirect("login")
+    }
+})
+
+
+app.post("/login",function(req,res){
+
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
+
+    req.login(user,function(err){
+        if(err){
+            console.log(err)
+        }else{
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secret")
+            })
+        }
+    })
 
 })
 
-app.post("/login",function(req,res){
-    User.find({username: req.body.username}).then((data) => {
-        if(data[0]){
-            bcrypt.compare(req.body.password, data[0].password, function(err, result) {
-                if(result == true){
-                    res.render("secrets")
-                }else{
-                    // console.log("error")
-                    res.send("Error")
-                }
-            });
+app.get("/logout",function(req,res){
+    req.logout(function(err){
+        if(err){
+            console.log(err)
         }else{
-            res.send("Error")
+            res.redirect("/")
         }
     })
 })
